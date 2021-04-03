f6547781c8f72ebcf123b5759dacaf3cf2d83038
nguyenlamquocty1412 <nguyenlamquocty1412@gmail.com>
2021-04-03 15:54:04 +0700

    Solve conflict

--- CMP210-Practice/Practice-02/EmployeeService.cs
+++ CMP210-Practice/Practice-02/EmployeeService.cs
@@ -27,6 +27,9 @@ namespace Practice_02
                 case EmployeeType.SeniorManager:
                     factor = 2;
                     break;
+                case EmployeeType.Expert;
+                    factor = 3;
+                    break;
             }
 
             var salary = BASIC_SALARY * years * factor;
